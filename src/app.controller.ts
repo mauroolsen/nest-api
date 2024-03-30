92c1b0bdcbee3db848b7cee9c5207a2e2c3365a8
Mauro Olsen <mauro@techforb.com>
2024-03-30 12:09:51 -0300

    adding secrets

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/secret')
+  getSecret(): string {
+    return this.appService.getSecret();
+  }
 }
